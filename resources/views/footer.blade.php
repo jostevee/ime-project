150d847da0ff204453f0e7dd048d81287dec332d
jostevee <steveeejooo@gmail.com>
2021-09-07 10:22:07 +0700

    Update UI v2.13.24 Commit

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -15,15 +15,15 @@
             </div>
           </div> -->
           
-          <div class="col-lg-6 row d-flex justify-content-stretch">
+          <div class="col-lg-12 row d-flex justify-content-stretch">
             <div class="col-12">
               <h4 class="yellow-strong">IME Social Media</h4>
             </div>
             <div class="col-12 row">
               <div class="col-6 col-lg-3 sponsors d-flex justify-content-center align-items-center" onclick="location.href='https://instagram.com/marinexhibition.id/';"><i class="fab fa-instagram"></i></div>
               <div class="col-6 col-lg-3 sponsors d-flex justify-content-center align-items-center" onclick="location.href='mailto:ime.himiteka@gmail.com';"><i class="fas fa-envelope"></i></div>
-              <div class="col-6 col-lg-3 sponsors d-flex justify-content-center align-items-center"><i class="fas fa-cog"></i></div>
-              <div class="col-6 col-lg-3 sponsors d-flex justify-content-center align-items-center"><i class="fas fa-cog"></i></div>
+              <!-- <div class="col-6 col-lg-3 sponsors d-flex justify-content-center align-items-center"><i class="fas fa-cog"></i></div> -->
+              <!-- <div class="col-6 col-lg-3 sponsors d-flex justify-content-center align-items-center"><i class="fas fa-cog"></i></div> -->
             </div>
           </div>
 
